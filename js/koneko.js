86b6a962d6a97119923ed7b7c41c8108a5ca2d4c
Felix C. Stegerman <flx@obfusk.net>
2019-11-18 06:48:13 +0100

    ...

--- js/koneko.js
+++ js/koneko.js
@@ -1,13 +1,14 @@
 "use strict";
 
 // TODO:
+//  * pair (key, value)
 //  * multi (arity, name, table)
 //  * record-type (name, fields)
 //  * record (type, values)
 
 // TODO: inefficient
 const stack = {
-  new: () => [],
+  empty: () => [],
   push: (s, ...args) => s.concat(args),
   pop: (s0, ...args) => {
     if (s0.length < args.length) {
@@ -51,7 +52,13 @@ const scope = {
   },
 }
 
+// TODO
+const isIdent = s => !!s.match(/^[\p{L}\p{N}\p{S}]+$/u)
+
 const pushSelf = v => (c, s) => stack.push(s, v)
+
+const pushIdent = v => (c, s) =>
+  stack.push(s, scope.lookup(c, v.value))
 
 const popArgs = (s0, b) => {
   const ps = {}, [vs, s1] = stack.popN(s0, b.params.length)
@@ -63,7 +70,7 @@ const popArgs = (s0, b) => {
 
 const arith = op => (c, s0) => {
   const [[x, y], s1] = stack.pop(s0, "int", "int")
-  return stack.push(s1, op(x.value, y.value))
+  return stack.push(s1, { type: "int", value: op(x.value, y.value) })
 }
 
 // TODO
@@ -75,29 +82,84 @@ const call = (c, s0) => {
       const [ps, s2] = popArgs(s0, x)
       const c1 = x.params.length ? scope.fork(x.scope, ps) : x.scope
       return evaluate(x.code)(c1, s2)
-      break
     case "builtin":
       return x.run(c, s1)
-      break
     // multi recordt record
     default:
       throw `uncallable: ${x.type}`
   }
 }
 
-// TODO
-const run = {
-  // nil bool
-  int: pushSelf,
-  // float str kwd pair list dict
+const evaluate = code => (c, s) => {
+  for (let x of code) {
+    s = evl[x.type](x)(c, s)
+  }
+  return s
+}
+
+const evl = {
+  nil: pushSelf, bool: pushSelf, int: pushSelf, float: pushSelf,
+  str: pushSelf, kwd: pushSelf,
+  list: v => (c, s) => {
+    const l = evaluate(v)(c, stack.empty()); l.reverse()
+    return stack.push({ type: list, value: l })
+  },
   ident: v => (c, s) =>
-    modules.__prim__.__call__.run(
-      c, stack.push(s, scope.lookup(c, v.value))
-    ),
-  // quot
+    modules.__prim__.__call__.run(c, pushIdent(v)(c, s)),
+  quot: pushIdent,
   block: v => (c, s) =>
     stack.push(s, { ...v, scope: c }),
-  // builtin multi recordt record
+}
+
+// TODO
+const show = (v) => {
+  switch (v.type) {
+    case "nil":
+      return "nil"
+    case "bool":
+      return v.value ? "#t" : "#f"
+    case "int":
+      return v.value.toString()
+    case "float":
+      return v.value.toString()                               //  TODO
+    case "str":
+      return JSON.stringify(v.value)                          //  TODO
+    case "kwd":
+      return ":" + (isIdent(v.value) ? v.value
+                    : JSON.stringify(v.value))                //  TODO
+    case "pair":
+      return show(v.key) + " " + show(v.value) + " =>"
+    case "list":
+      return v.value.length ?
+        "( " + v.value.map(show).join(" ") + " )" : "()"
+    case "dict": {
+      const f = k =>
+        show({ type: "kwd", value: k.slice(1) /* :key */ }) +
+          " " + show(v[k]) + " =>"
+      return v.value.length ?
+        "{ " + Object.keys(v.value).map(f).join(", ") + " }" : "{ }"
+    }
+    case "ident":
+      return v.value
+    // quot
+    case "block": {
+      const f = xs => xs.map(show).join(" ")
+      if (!v.params.length && !v.code.length) {
+        return "[ ]"
+      } else if (!v.params.length) {
+        return "[ " + f(v.code) + " ]"
+      } else if (!v.code.length) {
+        return "[ " + v.params.join(" ") + " . ]"
+      } else {
+        return "[ " + v.params.join(" ") + " . " + f(v.code) + " ]"
+      }
+    }
+    case "builtin":
+      return "#<" + (v.prim ? "primitive" : "builtin") + ":" + v.name + ">"
+    // multi recordt record
+    default:
+      throw `unshowable: ${v.type}`
+  }
 }
 
 // TODO
@@ -140,13 +202,6 @@ for (let k of Object.keys(modules.__prim__)) {
   modules.__prim__[k].prim = true
 }
 
-const evaluate = code => (c, s) => {
-  for (let x of code) {
-    s = run[x.type](x)(c, s)
-  }
-  return s
-}
-
 const example = [
   {
     type: "int",
@@ -174,10 +229,12 @@ const example = [
 ]
 
 const test = () =>
-  evaluate(example)(scope.new(), stack.new())
+  evaluate(example)(scope.new(), stack.empty())
 
 module.exports = { modules, example, test }
 
 if (require.main === module) {
-  console.log(test())
+  console.log(
+    example.map(show).join(" "), "\n=>", test().map(show).join(" ")
+  )
 }
