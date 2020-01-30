5c7616871c255b58fb4f4a3992c6997bff52d408
Felix C. Stegerman <flx@obfusk.net>
2020-01-30 23:54:07 +0100

    scripts: s/type/command -v/ (suppress error)

--- js/cli.sh
+++ js/cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rlwrap=()
-if type rlwrap >/dev/null; then
+if command -v rlwrap >/dev/null; then
   rlwrap=( rlwrap -C koneko )
 fi
 exec "${rlwrap[@]}" node -e 'require("koneko").main()' _ "$@"
